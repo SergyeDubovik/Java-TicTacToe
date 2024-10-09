c1e4d4da5dcab55a796c0f891220a18bb4a5f572
Serg <supermirko9@gmail.com>
2024-10-09 22:39:10 +0300

    formatted code

--- src/AI.java
+++ src/AI.java
@@ -1,7 +1,7 @@
 public class AI {
 //NOT YET USED
 //easy - will make losing moves
-//medium - will try to make a tictactoe
-//hard - will block other player's tictactoe
+//medium - will try to make a ticTacToe
+//hard - will block other player's ticTacToe
 //stupid - will make random moves (easiest to code)
 }
